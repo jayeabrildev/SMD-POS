188fffbb039cdfbe0f7284a59ca62d4089639e22
Jaye Abril <jayeabril.developer@gmail.com>
2021-11-08 05:21:58 +0800

    Project Cleanup

--- SMD Water Station/Backend/Models/Sales_DateModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SMD_Water_Station.Backend.Models
-{
-    public class Sales_DateModel
-    {
-        public System.DateTime DateTime { get; set; }
-        public double Value { get; set; }
-    }
-}
